9e50bf5f8f04f8531234680a00450ed52de046e2
DerZade <DerZade@gmail.com>
2022-07-07 15:49:18 +0200

    Add erroring code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { assert } from "@vue/compiler-core";
 import { useCounter, } from "./stores/counter";
 
-const counter = useCounter()
+const counter = useCounter();
+
+// Typescript says, that counter.nPlusX is a number,
+// but in fact it is a function, as this assert shows
+assert(typeof counter.nPlusX === 'function', 'counter.nPlusX is not a function'); // this does not throw
 </script>
 
 <template>
   <p>The counter is at {{ counter.n }}</p>
+  <p>The counter + 5 is at {{ counter.nPlusX(5) }}</p>
   <button @click="counter.n++">Increment</button>
   <button @click="counter.n--">Decrement</button>
   <button @click="counter.n = 0">Reset</button>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,7 +1,17 @@
+import { ref, computed } from 'vue';
 import { defineStore, acceptHMRUpdate } from 'pinia'
 
-export const useCounter = defineStore('counter', {
-  state: () => ({ n: 0 }),
+export const useCounter = defineStore('counter', () => {
+  const n = ref(0);
+
+  const nPlusOne = computed(() => n.value + 1);
+  const nPlusX = computed(() => (x: number) => n.value + x);
+
+  return {
+    n,
+    nPlusOne,
+    nPlusX
+  }
 })
 
 if (import.meta.hot) {
